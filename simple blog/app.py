a2375bead2f4c5c39548c8ad9af2c43b72598bbe
Muhammad Zaid <143324231+mzaid129@users.noreply.github.com>
2025-11-14 17:38:45 +0500

    Add files via upload

--- simple blog/app.py
+++ simple blog/app.py
@@ -49,5 +49,28 @@ def view_post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', post=post)
 
+@app.route('/edit/<int:post_id>', methods=['GET', 'POST'])
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+
+        db.session.commit()
+        return redirect(url_for('view_post', post_id=post.id))
+
+    return render_template('edit_post.html', post=post)
+
+
+@app.route('/delete/<int:post_id>', methods=['POST'])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
